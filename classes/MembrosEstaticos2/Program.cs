c3a4597f5ba413362e89329dc513e5b42dff4836
WilliamAlvesOliveira <27will.ramone@gmail.com>
2026-04-02 10:42:44 -0300

    Save Updates

--- classes/MembrosEstaticos2/Program.cs
+++ classes/MembrosEstaticos2/Program.cs
@@ -1,42 +1,38 @@
 ﻿using System;
 using System.Globalization;
 
-namespace MembrosEstaticos_pt1;
-
-class Program
+namespace MembrosEstaticos_pt2
 {
-    const double PI = 3.14;
+    /* 
+     * Classes estáticas ou membros estáticos são úteis quando:
+     * - Não há necessidade de criar instâncias (objetos) da classe.
+     * - Os métodos e propriedades representam comportamentos ou valores universais.
+     * - Exemplo: cálculos matemáticos, constantes físicas, utilitários de conversão.
+     */
 
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Entre com o valor do raio: ");
-        double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-        double circunferencia = Circunferencia(raio);
+            // Chamando métodos estáticos diretamente pela classe
+            double circunferencia = Calculadora.Circunferencia(raio);
+            Console.WriteLine(
+                $"A circunferência do raio {raio} é {circunferencia.ToString("F2", CultureInfo.InvariantCulture)}"
+            );
 
-        Console.WriteLine(
-            $"A circunferência do raio {raio} é {circunferencia.ToString("F2", CultureInfo.InvariantCulture)}"
-        );
+            double volume = Calculadora.Volume(raio);
+            Console.WriteLine(
+                $"Volume da esfera de raio {raio} é {volume.ToString("F2", CultureInfo.InvariantCulture)}"
+            );
 
-        double volume = Volume(raio);
-
-        Console.WriteLine(
-            $"Volume da esfera de raio {raio} é {volume.ToString("F2", CultureInfo.InvariantCulture)}"
-        );
-
-        Console.WriteLine(
-            "O valor de PI é " + PI.ToString("F2", CultureInfo.InvariantCulture)
-        );
+            // Acessando constante estática
+            Console.WriteLine(
+                "O valor de PI é " + Calculadora.PI.ToString("F2", CultureInfo.InvariantCulture)
+            );
+        }
     }
 
-
-    static double Circunferencia(double raio)
-    {
-        return 2.0 * PI * raio;
-    }
-
-    static double Volume(double raio)
-    {
-        return 4.0 / 3.0 * PI * raio * raio * raio;
-    }
 }
