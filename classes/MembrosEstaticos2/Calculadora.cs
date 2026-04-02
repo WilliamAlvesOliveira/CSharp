0d7d91c8c95ae938edbf4fd8062f37152eb49cfb
WilliamAlvesOliveira <27will.ramone@gmail.com>
2026-04-02 10:39:32 -0300

    feat: adicionar aula classes MembrosEstaticos2

---
+++ classes/MembrosEstaticos2/Calculadora.cs
@@ -0,0 +1,20 @@
+﻿namespace MembrosEstaticos_pt2
+{
+    class Calculadora
+    {
+        // Constante estática: valor fixo e imutável, acessível sem instanciar a classe
+        public const double PI = 3.14159;
+
+        // Método estático: não depende de atributos de instância
+        public static double Circunferencia(double raio)
+        {
+            return 2.0 * PI * raio;
+        }
+
+        public static double Volume(double raio)
+        {
+            return 4.0 / 3.0 * PI * Math.Pow(raio, 3);
+        }
+    }
+
+}
---
+++ classes/MembrosEstaticos2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace MembrosEstaticos_pt1;
+
+class Program
+{
+    const double PI = 3.14;
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Entre com o valor do raio: ");
+        double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        double circunferencia = Circunferencia(raio);
+
+        Console.WriteLine(
+            $"A circunferência do raio {raio} é {circunferencia.ToString("F2", CultureInfo.InvariantCulture)}"
+        );
+
+        double volume = Volume(raio);
+
+        Console.WriteLine(
+            $"Volume da esfera de raio {raio} é {volume.ToString("F2", CultureInfo.InvariantCulture)}"
+        );
+
+        Console.WriteLine(
+            "O valor de PI é " + PI.ToString("F2", CultureInfo.InvariantCulture)
+        );
+    }
+
+
+    static double Circunferencia(double raio)
+    {
+        return 2.0 * PI * raio;
+    }
+
+    static double Volume(double raio)
+    {
+        return 4.0 / 3.0 * PI * raio * raio * raio;
+    }
+}
