9d2b9642cf1a21ba4b27f819165c8cbb8d77ae55
WilliamAlvesOliveira <27will.ramone@gmail.com>
2026-04-05 12:26:51 -0300

    feat:
    cria classe Verificador no Service

---
+++ projetos/Jokenpo/Service/VerificarVencedor.cs
@@ -0,0 +1,40 @@
+﻿using Jokenpo.View.TableRender;
+using static Jokenpo.Models.Colors;
+
+
+
+namespace Jokenpo.Service
+{
+    public static class Verificador
+    {
+        public static (string vencedor, int cpu)
+            VerificarVencedor(int option, double time)
+        {
+
+            Random rnd = new Random();
+            int computer = rnd.Next(1, 4);
+
+            Render.Text($"opção {option} tempo {time}");
+            Render.DrawLine('=');
+
+            if (option == -1)
+            {
+                return ("Timeout", computer);
+            }
+            else if (option == computer)
+            {
+                return ("Empate", computer);
+            }
+            else if (option == 1 && computer == 3 ||
+                option == 2 && computer == 1 ||
+                option == 3 && computer == 2)
+            {
+                return ("Player", computer);
+            }
+            else
+            {
+                return ("CPU", computer);
+            }
+        }
+    }
+}
