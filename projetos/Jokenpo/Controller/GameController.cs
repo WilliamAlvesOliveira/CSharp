c76782bc8fcc6f8b370d3d4aa8efa913529d4f5f
WilliamAlvesOliveira <27will.ramone@gmail.com>
2026-04-05 12:22:54 -0300

    Feat:
    cria classe ap e implemente loop de jogo

---
+++ projetos/Jokenpo/Controller/GameController.cs
@@ -0,0 +1,27 @@
+﻿using Jokenpo.View.Game;
+using Jokenpo.View.Menus;
+using Jokenpo.View.TableRender;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jokenpo.Controller
+{
+    public class GameController
+    {
+        public static void APP()
+        {
+            MenuPrincipal.MenuScreen();
+            while (true)
+            {
+                GameScreen.GamePlay();
+                int restart = GameScreen.RestartMessage();
+
+                if (restart != 1) break;
+            }
+            
+        }
+    }
+}
