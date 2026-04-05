93cdfde6366b81a9d3e42a3fde13237489bde1e3
WilliamAlvesOliveira <27will.ramone@gmail.com>
2026-04-05 12:24:36 -0300

    feat:
    Adiciona opções de background na classe Colors

--- projetos/Jokenpo/Models/Colors.cs
+++ projetos/Jokenpo/Models/Colors.cs
@@ -1,7 +1,8 @@
-﻿namespace Jokenpo.Models
+﻿
+namespace Jokenpo.Models
 {
     public class Colors
-    {
+     {
         public const string RESET = "\u001b[0m";
         public const string RED = "\u001b[31m";
         public const string GREEN = "\u001b[32m";
@@ -9,5 +10,8 @@
         public const string BLUE = "\u001b[34m";
         public const string CYAN = "\u001b[36m";
         public const string BOLD = "\u001b[1m";
-    }
+        public const string BG_RED = "\u001b[41m";
+        public const string BG_WHITE = "\u001b[47m";
+     }
+
 }
