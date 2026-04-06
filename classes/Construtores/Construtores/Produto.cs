2c274e5db384dd42dd09a246db6724e1fe688599
WilliamAlvesOliveira <27will.ramone@gmail.com>
2026-04-06 13:20:43 -0300

    feat:
    Adicona aula sobre construtors e sobrecarga.

---
+++ classes/Construtores/Construtores/Produto.cs
@@ -0,0 +1,136 @@
+﻿using static System.Globalization.CultureInfo;
+
+namespace Construtores
+{
+    public class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Qtd;
+
+        public void DadosDoProduto()
+        {
+            Console.WriteLine($"Produto: {Nome}\nPreço: {Preco.ToString("F2", InvariantCulture)}\nQuantidade: {Qtd}");
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome}, $ {Preco.ToString("F2", InvariantCulture)}, {Qtd} unidades, total: {ValorTotalEstoque().ToString("F2", InvariantCulture)}";
+        }
+
+        public double ValorTotalEstoque()
+        {
+            return Preco * Qtd;
+        }
+
+        public void AdicionarProduto(int qtd)
+        {
+            if (qtd >= 0)
+            {
+                Qtd += qtd;
+            }
+            else
+            {
+                Console.WriteLine("Quantidade Inválida!");
+                Console.Write("\n");
+            }
+        }
+
+        public void RemoverProduto(int qtd)
+        {
+            if (qtd >= 0 && qtd <= Qtd)
+            {
+                Qtd -= qtd;
+            }
+            else
+            {
+                Console.WriteLine("Quantidade Inválida!");
+                Console.Write("\n");
+            }
+        }
+    }
+
+    public class ProdutoWithConstructor
+    {
+        public string Nome;
+        public double Preco;
+        public int Qtd;
+
+        /*
+         * CONSTRUTOR DA CLASSE:
+         * 
+         * O construtor é um método especial que possui o MESMO NOME da classe.
+         * Ele é chamado automaticamente no momento em que criamos um objeto (new).
+         * 
+         * Diferente dos métodos comuns:
+         * - Não possui tipo de retorno (nem mesmo void)
+         * - Serve para inicializar o objeto com valores obrigatórios
+         */
+        public ProdutoWithConstructor(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Qtd = quantidade;
+        }
+
+        /*
+         * Construtor simplificado:
+         * 
+         * Neste caso, o usuário informa apenas nome e preço.
+         * A quantidade é definida automaticamente com um valor padrão (1).
+         * 
+         * Isso é útil para facilitar a criação de objetos quando nem todos
+         * os dados precisam ser informados manualmente.
+         */
+
+        //O conceito de mais de um construtor é a SOBRECARGA.
+
+        public ProdutoWithConstructor(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+            Qtd = 1;
+        }
+
+        public void DadosDoProduto()
+        {
+            Console.WriteLine($"Produto: {Nome}\nPreço: {Preco.ToString("F2", InvariantCulture)}\nQuantidade: {Qtd}");
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome}, $ {Preco.ToString("F2", InvariantCulture)}, {Qtd} unidades, total: {ValorTotalEstoque().ToString("F2", InvariantCulture)}";
+        }
+
+        public double ValorTotalEstoque()
+        {
+            return Preco * Qtd;
+        }
+
+        public void AdicionarProduto(int qtd)
+        {
+            if (qtd >= 0)
+            {
+                Qtd += qtd;
+            }
+            else
+            {
+                Console.WriteLine("Quantidade Inválida!");
+                Console.Write("\n");
+            }
+        }
+
+        public void RemoverProduto(int qtd)
+        {
+            if (qtd >= 0 && qtd <= Qtd)
+            {
+                Qtd -= qtd;
+            }
+            else
+            {
+                Console.WriteLine("Quantidade Inválida!");
+                Console.Write("\n");
+            }
+        }
+    }
+}
